71f7518a356b9ee82e0fda544b99a63e1cca3349
kalinkochnev <kalin.kochnev@gmail.com>
2019-06-21 19:16:10 -0400

    remade command terminal and pid finding code

--- GameManager.py
+++ GameManager.py
@@ -1,130 +1,155 @@
+import queue 
 import psutil
-import datetime
 import time
-import json
-import os
-
-class GameManager():
-    def __init__(self, sessionClass, killClass, gameName):
-        self.Session = sessionClass
-        self.Kill = killClass
-        self.trackedGame = gameName
-        self.currentlyRunning = []
-    
-    def watch(self):
-        while True:
-            self._addCurrentlyRunning()
-            self._removeNotRunning()
-            time.sleep(10)
-
-    def _checkGameInfo(self, mode):
-        if mode == "status":
-            for proc in psutil.process_iter() :
-                if self.trackedGame == proc.name():
-                    return True
-            return False
-
-        if mode == "creationTime":
-            gamePids = []
-            #get pids for the game
-            for proc in psutil.process_iter(attrs=['name', 'pid']):
-                if proc.info['name'] == self.trackedGame:
-                    gamePids.append(proc.info['pids'])
-            
-            p = psutil.Process()
-            lowestTime = int(p.oneshot().create_time(gamePids[0]))
-            for pid in gamePids:
-                if p.oneshot().create_time(pid) < lowestTime:
-                    lowestTime = int(p.oneshot().create_time(pid))
-            return lowestTime
-        
-    def _addCurrentlyRunning(self):
-        if _checkGameInfo("status") == True and self.trackedGame not in self.currentlyRunning:
-            self.currentlyRunning.append(self.trackedGame)
-            gameStartTime = _checkGameInfo("creationTime")
-            self.Session.start(gameStartTime)
-
-    def _removeNotRunning(self):
-        if _checkGameInfo("status") == False and self.trackedGame in self.currentlyRunning:
-            self.currentlyRunning.remove(self.trackedGame)
-            self.Session.close()
-    
-    def userInteract(self):
+import re
+import threading
+import tkinter
 
 
-class Sessions():
-    def __init__(self, manageDataClass, gameName):
-        self.manageData = manageDataClass
-        self.trackedGame = gameName
-        
-        self.timeToPause = 0
-        self.pausesUsed = 0
+namesOfGames = ['calculator', 'discord']
 
-        self.gameStartTime = 0
-        self.gameEndTime = 0
-        self.currentDate = datetime.now.strftime("%m/%d/%Y")
+GamesTracking = []
+runningGames = []
+timeLimit = 100
+commandQueue = queue.Queue()
+timePause = 10
 
-    def start(self, timeCreated):
-        self.gameStartTime = timeCreated
-    def close(self):
-        self.gameEndTime = int(time.time())
-    def pause(self, timeToPause):
-        self.timeToPause += timeToPause
-        self.pausesUsed += 1
+class GameObject():
+    gameName = "" 
+    gamePids = []
+    elapsedTime = 0
+    status = ""
 
-    def storeData(self):
-        jsonText = {
-            "gameName":self.trackedGame,
-            "times": {
-                "startTime":self.gameStartTime,
-                "endTime":self.gameEndTime,
-                "duration":self.gameEndTime-self.gameStartTime 
-            },
-            "pausesUsed" : self.pausesUsed,
-        }
-        self.manageData.storeData(json.dumps(jsonText))
-    def loadData(self):
-
-class manageData():
-    def __init__(self, jsonData):
-        self.jsonData = jsonData
-        
-    def storeData(self):
-        
-    def accessDayData(self):
-    def accessRangeData(self):
-
-
-class killGame():
     def __init__(self, gameName):
-        self.trackedGame = gameName
+        self.gameName = gameName
 
-    def killGame(self):
-        gamePids = []
-        #get pids for the game
-        for proc in psutil.process_iter(attrs=['name', 'pid']):
-            if proc.info['name'] == self.trackedGame:
-                gamePids.append(proc.info['pids'])
+    def update_pids(self):
+        currentPids = []
+        #iterate through processes
+        for process in psutil.process_iter(attrs=['name', 'pid']):
+            #if processName contains gameName
+            if self.gameName.lower() in process.info['name'].lower():
+                #get pid of process and add to currentPids list
+                currentPids.append(process.info['pid'])
+                self.status = 'running'
+                print("%s has been found with pid: %s" % (self.gameName, process.info['pid']))
+                self.gamePids = currentPids
+        #if num pids is 0, status = closed
+        if len(currentPids) == 0:
+            status = 'closed'
+            print("%s is closed" % self.gameName)
+
+    def get_pids(self):
+        return self.gamePids
+
+    def clear_pids(self):
+        self.gamePids = []
+
+    def inc_game_runtime(self, timeToIncrease):
+        self.elapsedTime += timeToIncrease
+
+    def get_status(self):
+        return self.status
+
+    def get_name(self):
+        return self.gameName
+
+"""
+class GUI():
+    def Interface(self):
+        GameManager = tkinter.Tk()
+        time_entry = tkinter.Entry(GameManager).grid(row=0)
+
+        def getInput():
+            user_input = time_entry.get()
+            if re.search("\d+$", user_input) != None:
+                self.sendCommand()
+
+        #TODO 
+        close_games = tkinter.Button(GameManager, text="Close all", width=50, command=getInput).grid(row=1)
+        add_time = tkinter.Button(GameManager, text="Add time", width=50, command=getInput).grid(row=2)
         
-        p = psutil.Process()
-        lowestTime = p.oneshot().create_time(gamePids[0])
-        for pid in gamePids:
+        GameManager.mainloop()
+    
+    def sendCommand(self, name, info):
+        #commandQueue.put({name, info})
+        print("command sent")
+"""
+
+def command_interface():
+    global timeLimit
+    print("Welcome to the game manager!")
+    print("1.   add time [time in minutes]")
+    print("2.   close all")
+    print("Please input your commands:")
+    while True:
+        #gets user input and checks it against the regex for the command
+        userInput = input()
+        if  re.search("^add time{1} \d+", userInput) != None:
+            increaseBy = re.findall('\d+', userInput)
+            commandQueue.put({'add time' : int(increaseBy[0])})
+        elif re.search("^close all{1}", userInput) != None:
+            for game in runningGames:
+                commandQueue.put({'stop' : game.get_pids()})
+
+
+def command_executor():
+    global timeLimit
+    #executes commands put through the queue
+    command = commandQueue.get()
+    #checks if the dict key is the right one
+    if "stop" in command:
+        #loop through pids and kill them
+        #TODO make sure it has pids to kill, if not find them
+        for pid in command["stop"]:
             psutil.Process(pid).kill()
+        """
+        TODO
+            -stop the running timer
+            -store the session data somewhere
+        """
+
+    if "add time" in command:
+        timeLimit += command["add time"]
+        print("The time was increased by %s minutes" % command['add time'])
+        print(timeLimit)
+
+
 
 if __name__ == "__main__":
-    """
-    overwatch = game(1, "Discord")
-    
-    testOpen = Process(target=overwatch.isGameOpen, args=())
-    timer = Process(target=overwatch.timer, args=())
-    track = Process(target=overwatch.trackGame, args=())
-    
-    testOpen.start()
-    timer.start()
-    track.start()
+    #TODO terminal only working with first command inputted
+    #create a command_interface and a command_executor thread
+    interfaceThread = threading.Thread(target=command_interface)
+    interfaceThread.start()
 
-    overwatch.interactWithUser()
+    #create array of objects for each game user wants to track
+    for name in namesOfGames:
+        GamesTracking.append(GameObject(name))
+        #gets initial PID states for object
+        GamesTracking[len(GamesTracking)-1].get_pids()
 
+    totalTime = 0
     while True:
-        time.sleep(10)
-    """
+        time.sleep(timePause)
+        command_executor()   
+
+        #check if each item in gamesTracking is running
+        for game in GamesTracking:
+            game.update_pids()
+            #if yes, add to running games
+            if game.get_status() == "running" and game not in runningGames:
+                runningGames.append(game)
+            #if game not running, remove from running games
+            if game.get_status() == 'closed':
+                runningGames.remove(game)
+            #check if total time is greater than timeLimit
+            if totalTime > timeLimit and len(runningGames) > 0:
+                #send command into queue to kill the tracked game at the given PIDS
+                commandQueue.put({"kill" : game.get_pids()})
+                #resets the pids of the game and removes it from running games
+                if game in runningGames:
+                    game.clear_pids()
+                    runningGames.remove(game)
+        totalTime += timePause
+        game.inc_game_runtime(timePause)
+        print("Currently running games %s" % [game.get_name() for game in runningGames])
